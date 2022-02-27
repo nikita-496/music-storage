4dee678ca10b103df4c0c5f0d76ec9a300470701
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-02-27 12:01:23 +0500

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const startApp = async () => {
   try {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
   } catch(e) {
     console.log(e);
