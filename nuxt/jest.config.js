09e8adb9592d476c22595e655d1ce2b4c969d463
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-02-28 11:04:31 +0500

    Setting up client tests

--- nuxt/jest.config.js
+++ nuxt/jest.config.js
@@ -1,24 +1,21 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+    "^vue$": "vue/dist/vue.common.js",
   },
-  moduleFileExtensions: [
-    'ts',
-    'js',
-    'vue',
-    'json'
-  ],
+  moduleFileExtensions: ["ts", "js", "vue", "json"],
+  rootDir: "client",
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest",
   },
   collectCoverage: true,
+  coverageDirectory: "../coverage-client",
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    "<rootDir>/components/**/*.vue",
+    "<rootDir>/pages/**/*.vue",
   ],
-  testEnvironment: 'jsdom'
-}
+  testEnvironment: "jsdom",
+};
