7ca61cd99e6dcd53dccdaab908ed09e8a054e38d
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-03-07 15:45:42 +0500

    Scss connection

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -22,7 +22,7 @@ const config = async () => ({
   serverMiddleware: isDev ? [] : [{ path: '/api', handler: await bootstrap() }],
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['./client/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
