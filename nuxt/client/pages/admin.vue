cc39e443e349d83cb0b8a268d3fe3d17f37ce31f
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-03-10 07:30:10 +0500

    Create an admin page

---
+++ nuxt/client/pages/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <admin-editor />
+</template>
+
+<script lang="ts">
+import AdminEditor from '../components/form/AdminEditor.vue';
+import { Component, Vue } from 'nuxt-property-decorator';
+@Component({
+  components: {
+    AdminEditor
+  }
+})
+export default class Administartion extends Vue {}
+</script>
+
+<style scoped></style>
