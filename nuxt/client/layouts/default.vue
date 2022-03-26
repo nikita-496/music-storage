143bd5d20a8226fd2abe56b405290212a546724e
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-03-26 13:58:08 +0500

    Add layout header

---
+++ nuxt/client/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header class="header">
+      <ul class="header__menu">
+        <li class="header__menu-item">
+          <NuxtLink class="header__link" to="/">Главная</NuxtLink>
+        </li>
+        <li class="header__menu-item">
+          <NuxtLink class="header__link" to="/admin"
+            >Панель администратора</NuxtLink
+          >
+        </li>
+        <li class="header__menu-item">
+          <NuxtLink class="header__link" to="">Регистрация</NuxtLink>
+        </li>
+        <li class="header__menu-item">
+          <NuxtLink class="header__link" to="">Войти</NuxtLink>
+        </li>
+      </ul>
+    </header>
+    <main class="main"></main>
+    <footer class="footer"></footer>
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background-color: rgb(35, 169, 2422);
+}
+</style>
