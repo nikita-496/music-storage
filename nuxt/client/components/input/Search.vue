a2bf89dbf2ff2e0e5e8504d9b93dad17623848f4
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-04-12 16:08:07 +0500

    Search field component created

---
+++ nuxt/client/components/input/Search.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <img
+      class="search__image"
+      src="../../assets/images/icons/search.svg"
+      alt="search"
+    />
+    <input
+      class="search__input"
+      type="text"
+      placeholder="Композиция, альбом, исполнитель ..."
+    />
+  </div>
+</template>
+
+<script>
+import { Prop, Component, Vue, Watch, Emit } from 'nuxt-property-decorator';
+export default class Search extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/_vars.scss';
+
+.search__image {
+  position: relative;
+  top: 3px;
+  left: 40px;
+  width: 20px;
+  height: 20px;
+}
+.search__input {
+  width: 20em;
+  padding: 0.9em 1.5em;
+  padding-left: 3em;
+  border: 1px solid transparent;
+  border-radius: 6px;
+  outline: none;
+}
+.search__input::placeholder {
+  font-family: $Lato;
+  font-size: 0.9rem;
+
+  color: $black;
+  opacity: 0.6;
+}
+input::-webkit-input-placeholder {
+  opacity: 1;
+  transition: opacity 0.4s ease;
+}
+input::-moz-placeholder {
+  opacity: 1;
+  transition: opacity 0.4s ease;
+}
+input:-moz-placeholder {
+  opacity: 1;
+  transition: opacity 0.4s ease;
+}
+input:-ms-input-placeholder {
+  opacity: 1;
+  transition: opacity 0.4s ease;
+}
+input:focus::-webkit-input-placeholder {
+  opacity: 0;
+  transition: opacity 0.4s ease;
+}
+input:focus::-moz-placeholder {
+  opacity: 0;
+  transition: opacity 0.4s ease;
+}
+input:focus:-moz-placeholder {
+  opacity: 0;
+  transition: opacity 0.4s ease;
+}
+input:focus:-ms-input-placeholder {
+  opacity: 0;
+  transition: opacity 0.4s ease;
+}
+</style>
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -16,7 +16,13 @@ const config = async () => ({
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap'
+      }
+    ]
   },
 
   serverMiddleware: isDev ? [] : [{ path: '/api', handler: await bootstrap() }],
